574bd9f81362c289d2ce4dc787aca9e5b2d89d3e
shahadathossain4536 <shahadathossain4536@gmail.com>
2023-09-07 19:58:58 +0600

    fix_space

--- index.js
+++ index.js
@@ -3,52 +3,39 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 require("dotenv").config();
-
 const fs = require('fs');
-
-
-
 
 app.use(express.json());
 app.use(cors());
 
-
-
-
-
 async function run() {
   try {
     // await client.connect();
-
- 
-
-
   } finally {
 
-  
-      app.get('/script', (req, res) => {
-        // Read the JavaScript file
-        fs.readFile('./popup-form.js', 'utf8', (err, data) => {
-          if (err) {
-     
-            res.status(404).send('File not found');
-          } else {
-       
-            res.setHeader('Content-Type', 'application/javascript');
-          
-            res.send(data);
-          }
-        });
+    app.get('/script', (req, res) => {
+      // Read the JavaScript file
+      fs.readFile('./popup-form.js', 'utf8', (err, data) => {
+        if (err) {
+
+          res.status(404).send('File not found');
+        } else {
+
+          res.setHeader('Content-Type', 'application/javascript');
+
+          res.send(data);
+        }
       });
-      
+    });
+
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("manufacturers");
+  res.send("displayPopupForm_server");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port manufacturers ${port}`);
+  console.log(`displayPopupForm_server ${port}`);
 });
